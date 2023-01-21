13f43781532812e7bfd563c7adda16c599ce4e98
DevLaukey <devlaukeymwaura@gmail.com>
2023-01-21 23:23:16 +0300

    Added deploy scripts

--- .history/scripts/deploy_20230120235944.js
+++ .history/scripts/deploy_20230120235944.js
@@ -13,24 +13,14 @@ async function main() {
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const [deployer] = await ethers.getSigners();
   const Lock = await hre.ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
 
   await lock.deployed();
 
   console.log(
     `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
   );
-
-  console.log("Deploying contracts with the account:", deployer.address);
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  console.log("Token address:", token.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
